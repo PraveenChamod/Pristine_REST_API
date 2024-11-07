779c033c54608f2b9bc7062e1dca56679ce49cd4
Praveen Chamod <praveen.c@fidenz.com>
2024-11-07 22:31:17 +0530

    Configure services layer.

--- src/1 - Services/Pristine_REST_API.Api/Program.cs
+++ src/1 - Services/Pristine_REST_API.Api/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Pristine_REST_API.Infrastructure.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
