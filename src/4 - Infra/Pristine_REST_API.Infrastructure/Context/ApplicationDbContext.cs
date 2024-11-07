b3273484ea057475e0b73a587f52e05b079d0add
Praveen Chamod <praveen.c@fidenz.com>
2024-11-07 22:32:27 +0530

    Configure infrastructure layer.

---
+++ src/4 - Infra/Pristine_REST_API.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pristine_REST_API.Domain.Entities;
+
+namespace Pristine_REST_API.Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Employee>()
+                .HasIndex(e => e.EmpNo)
+                .IsUnique();
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.EmpAddressLine1)
+                .HasColumnType("nvarchar(100)")
+                .IsRequired();
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.EmpAddressLine2)
+                .HasColumnType("nvarchar(100)");
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.EmpAddressLine3)
+                .HasColumnType("nvarchar(100)");
+        }
+    }
+}
---
+++ src/4 - Infra/Pristine_REST_API.Infrastructure/Migrations/20241107165722_AddEmployeeEntity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pristine_REST_API.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmployeeEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    EmpNo = table.Column<string>(type: "varchar(10)", maxLength: 10, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    EmpName = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    EmpAddressLine1 = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    EmpAddressLine2 = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    EmpAddressLine3 = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    EmpDateOfJoin = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    EmpStatus = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    EmpImage = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_EmpNo",
+                table: "Employees",
+                column: "EmpNo",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
