66556a47607ee1c0e86fde4abdd2435764263aa9
Praveen Chamod <praveen.c@fidenz.com>
2024-11-07 22:31:52 +0530

    Configure domain layer.

---
+++ src/3 - Domain/Pristine_REST_API.Domain/Entities/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pristine_REST_API.Domain.Entities
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(10)]
+        public required string EmpNo { get; set; }
+
+        [MaxLength(100)]
+        public required string EmpName { get; set; }
+
+        [MaxLength(100)]
+        public required string EmpAddressLine1 { get; set; }
+
+        [MaxLength(100)]
+        public string? EmpAddressLine2 { get; set; }
+
+        [MaxLength(100)]
+        public string? EmpAddressLine3 { get; set; }
+
+        public required DateTime EmpDateOfJoin { get; set; }
+
+        public required bool EmpStatus { get; set; }
+
+        public required string EmpImage { get; set; }
+    }
+}
