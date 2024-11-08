cd6be4caf6f5f23db3b748930c315a92c4cdfed1
Praveen Chamod <praveen.c@fidenz.com>
2024-11-08 10:29:44 +0530

    Create the interface for employee repository.

---
+++ src/2 - Application/Pristine_REST_API.Application/Interfaces/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using Pristine_REST_API.Domain.Entities;
+
+namespace Pristine_REST_API.Application.Interfaces.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Employee CreateEmployee(Employee employee);
+        IQueryable<Employee> GetEmployees();
+        Task<Employee?> GetByEmployeeId(int employeeId);
+        Employee UpdateEmployee(Employee employee);
+        Task DeleteEmployee(int employeeId);
+    }
+}
