a3fc1b75d3e07309523742275f40306085f1151b
Praveen Chamod <praveen.c@fidenz.com>
2024-11-08 10:30:32 +0530

    Register the employee repository using transient.

--- src/1 - Services/Pristine_REST_API.Api/Program.cs
+++ src/1 - Services/Pristine_REST_API.Api/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Pristine_REST_API.Application.Interfaces.Repositories;
 using Pristine_REST_API.Infrastructure.Context;
+using Pristine_REST_API.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
     ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
 
+builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
 
 builder.Services.AddControllers();
 
